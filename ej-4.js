736c3175381155d44a724fb52177b2e3198a1f25
WaldoAriel <fernandez_waldo@hotmail.com>
2024-08-10 21:10:18 -0300

    cuatro

--- ej-4.js
+++ ej-4.js
@@ -8,39 +8,63 @@ aplicación (por ejemplo, "Juego", "Redes sociales", "Utilidad"). */
 
 
 
-let wa = {
-    nombre: 'WhatsApp',
-    categoria: 'Mensajería',
+let insta = {
+    nombre: 'Instagram',
+    categoria: 'Redes Sociales',
     tamaño: '3 MB',
     calificacion: 5
 };
-let spotify = {
-    nombre: 'Spotify',
-    categoria: 'Música',
+let candyCrush = {
+    nombre: 'Candy Crush',
+    categoria: 'Juegos',
     tamaño: '6 MB',
     calificacion: 3
 };
 let tiktok = {
     nombre: 'TikTok',
-    categoria: 'RRSS',
+    categoria: 'Redes Sociales',
     tamaño: '7 MB',
     calificacion: 4
 };
 let kahoot = {
     nombre: 'Kahoot!',
-    categoria: 'juegos',
+    categoria: 'Juegos',
     tamaño: '2 MB',
     calificacion: 5
 };
+let maps = {
+    nombre: 'Maps',
+    categoria: 'Utilidades',
+    tamaño: '9 MB',
+    calificacion: 4
+};
 
-//let apps = [wa, spotify, tiktok, kahoot];
+
+let menu;
+let continuar = true
+do {
+    menu = parseInt(prompt(`Ejecute una categoría:
+        1 - Juegos
+        2 - Redes Sociales
+        3 - Utilidades
+        4 - Salir del Menú
+        `))
+    switch (menu) {
+        case 1: alert(`en esta categoría encontrarás aplicaciones como ${kahoot.nombre}, ${candyCrush.nombre}, entre otras`);       
+            break;
+        case 2: alert(`en esta categoría encontrarás aplicaciones como ${tiktok.nombre}, ${insta.nombre}, entre otras`);       
+            break;
+        case 3: alert(`en esta categoría encontrarás aplicaciones como ${maps.nombre}, entre otras`);       
+            break;
+        case 4: alert(`Regrese pronto!...`);
+        continuar = false       
+            break;
     
-switch (key) {
-    case value:
-        
-        break;
+        default: alert('No hay categorías asociadas a esta opción')
+            break; 
+    };
+} while(continuar === true);
 
-    default: 
-        break;
-}
+
+
 
